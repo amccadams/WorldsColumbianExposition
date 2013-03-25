dce9f26f41cdc6803f874eefeb7974583c94657b
amccadams <amccadams@gmail.com>
2013-03-25 11:15:44 -0500

    moved image files to save space, js file for map is working, quiz implementation is working as standalone but not with index file yet

--- js/wce.js
+++ js/wce.js
@@ -218,18 +218,114 @@ $(function() {
 
 /////mapster javascript/////
 
-$('img').mapster({
+// $('img').mapster({
 
-fillOpacity: 0.4,
+// fillOpacity: 0.4,
+//         fillColor: "000000",
+//         stroke: true,
+//         strokeColor: "ffffff",
+//         strokeOpacity: 0.8,
+//         strokeWidth: 2,
+//         mapKey: 'name',
+        
+//         showToolTip:true,
+//        toolTipClose: ["tooltip-hover", "area-mouseout"],
+//         areas:[
+//         {
+//         	key:"A",
+//         	toolTip:"This is a Building", 
+//         },
+//         {
+//         	key:"B",
+//         	toolTip:"This is B Building",
+//         },
+//         {
+//         	key:"building",
+//         	toolTip:$("#ArtBuilding"),
+//         },
+//         {
+//         	key:"J",
+//         	toolTip: $("#buildingtest"),
+//         },
+//         ]
+//    });
+      
+
+      $('img').mapster({
+    fillOpacity: 0.4,
         fillColor: "000000",
         stroke: true,
         strokeColor: "ffffff",
         strokeOpacity: 0.8,
         strokeWidth: 2,
+    toolTipContainer: $('#tooltip-container'),
+    mapKey: 'data-key',
+    areas: [ {
+        key: 'all',
+        toolTip: true
+    }],
+    showToolTip: true,
+    onShowToolTip: function(data) {
+        if (this.href && this.href!='#') {
+            data.toolTip.html('<img src="'+this.href+'">')
+            ;
+        }
+        
+    }});
 
-  // mapKey: 'building'
-   });
-      
+
+      ///Quiz///
+
+     
+    var total = $(".question").length;
+    var score = 0;
+    $(".submit-answer-button").on("click", function () {
+        var question = $(this).parents(".question");
+        var answer_choices = question.find("input");
+        var submitted_answers = question.find("input:checked");
+        var incorrect_answers = false;
+        submitted_answers.each(function (index, element) {
+            if ($(element).hasClass("incorrect-answer")) {
+                incorrect_answers = true;
+            }
+        });
+        answer_choices.each(function (index, element) {
+            if ($(element).hasClass("correct-answer") && !$(element).is(":checked")) {
+                incorrect_answers = true;
+            }
+        });
+        if (incorrect_answers) {
+            $(question).find(".incorrect-alert").show();
+        } else {
+            $(question).find(".correct-alert").show();
+            score++
+        }
+        $(this).attr("disabled", "true");
+        $(question).find(".explanation").show();
+        $(question).find(".next-button").show();
+        $(".next-button").on("click", function () {
+            // hide current question
+            $(question).hide();
+            // show next question
+            $(question).next().show();
+        });
+        if ($(question).hasClass("last")) {
+            $(".score").find(".correct-answers").text(score);
+            $(".score").find(".total-questions").text(total);
+            $(".score").show();
+        }
+        $(".take-quiz-again").on("click", function () {
+            $(".question").hide();
+            $(".score").hide();
+            $(".next-button").hide();
+            $(".explanation").hide();
+            $(".correct-alert").hide();
+            $(".incorrect-alert").hide();
+            $(".submit-answer-button").removeAttr("disabled");
+            $(".question").eq(0).show();
+        });
+    });
+
 
 
 }); //end document ready
